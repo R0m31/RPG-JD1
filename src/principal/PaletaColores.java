7c32b9cdd81e2abbf44fa50ffbe75fe1f4882f5b
R0m31 <R0m31.RPG@gmail.com>
2016-10-30 15:22:32 -0400

    Create PaletaColores.java

---
+++ src/principal/PaletaColores.java
@@ -0,0 +1,60 @@
+package principal;
+
+import java.awt.Color;
+
+public class PaletaColores {
+
+	/*
+	// COLORES DB16
+	public static Color COLOR_BLANCO_VERDE = new Color(0xDEEED6);
+	public static Color COLOR_MAGENTA_NEGRO = new Color(0x140C1C);
+	public static Color COLOR_VIOLETA = new Color(0x442434);
+	public static Color COLOR_AZUL_BANDERA_SATURADO = new Color(0x30346D);
+	public static Color COLOR_GRIS = new Color(0x4E4A4E);
+	public static Color COLOR_MARRON_SATURADO = new Color(0x854C30);
+	public static Color COLOR_VERDE_OSCURO = new Color(0x346524);
+	public static Color COLOR_ROJO_SATURADO = new Color(0xD04648);
+	public static Color COLOR_GRIS_CLARO = new Color(0x757161);
+	public static Color COLOR_AZUL_CIELO = new Color(0x597DCE);
+	public static Color COLOR_NARANJA_SATURADO = new Color(0xD27D2C);
+	public static Color COLOR_AZUL_GRISASEO = new Color(0x8595A1);
+	public static Color COLOR_VERDE_CLARO = new Color(0x6DAA2C);
+	public static Color COLOR_ROSADO = new Color(0xD2AA99);
+	public static Color COLOR_CELESTE = new Color(0x6DC2CA);
+	public static Color COLOR_AMARILLO_SATURADO = new Color(0xDAD45E);	
+	*/	
+			
+	//COLORES 32DB	
+	public static Color COLOR_NEGRO = new Color(0x000000);
+	public static Color COLOR_BLANCO = new Color(0xFFFFFF);
+	public static Color COLOR_AZUL_OSCURO = new Color(0x222034);
+	public static Color COLOR_MAGENTA_OSCURO = new Color(0x45283C);
+	public static Color COLOR_MARRON_OSCURO = new Color(0x663931);
+	public static Color COLOR_MARRON = new Color(0x8F563B);
+	public static Color COLOR_NARANJA = new Color(0xDF7126);
+	public static Color COLOR_MARRON_CLARO = new Color(0xD9A066);
+	public static Color COLOR_PIEL = new Color(0xEEC39A);
+	public static Color COLOR_AMARILLO = new Color(0xFBF236);
+	public static Color COLOR_VERDE_MANZANA = new Color(0x99E550);
+	public static Color COLOR_VERDE = new Color(0x6ABE30);
+	public static Color COLOR_VERDE_TURQUESA = new Color(0x37946E);
+	public static Color COLOR_VERDE_OLIVA_OSCURO = new Color(0x4B692F);
+	public static Color COLOR_VERDE_MARRON = new Color(0x524B24);
+	public static Color COLOR_VERDE_MARRON_CLARO= new Color(0x8A6F30);
+	public static Color COLOR_VERDE_OLIVA = new Color(0x8F974A);
+	public static Color COLOR_AZUL_MARINO = new Color(0x3F3F74);
+	public static Color COLOR_AZUL_BANDERA = new Color(0x306082);
+	public static Color COLOR_AZUL = new Color(0x5B6EE1);
+	public static Color COLOR_AZUL_CLARO = new Color(0x639BFF);
+	public static Color COLOR_AZUL_TURQUESA = new Color(0x5FCDE4);
+	public static Color COLOR_AZUL_GRIS_CLARO = new Color(0xCBDBFC);
+	public static Color COLOR_GRIS_VERDE_OSCURO = new Color(0x323C39);
+	public static Color COLOR_GRIS_MEDIO_OSCURO = new Color(0x9BADB7);
+	public static Color COLOR_GRIS_VIOLETA_OSCURO = new Color(0x847E87);
+	public static Color COLOR_GRIS_MARRON_MEDIO_OSCURO = new Color(0x696A6A);
+	public static Color COLOR_GRIS_OSCURO = new Color(0x595652);
+	public static Color COLOR_MAGENTA = new Color(0x76428A);
+	public static Color COLOR_ROJO = new Color(0xAC3232);
+	public static Color COLOR_ROJO_CLARO = new Color(0xD95763);
+	public static Color COLOR_VIOLETA_CLARO = new Color(0xD77BBA);		
+}
